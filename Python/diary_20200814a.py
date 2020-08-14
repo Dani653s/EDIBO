71a14b9e48ffccc08223e9f6a5698b4dca810ab6
user <danielaveldze@gmail.com>
2020-08-14 16:49:22 +0300

    20200814_16_48

---
+++ Python/diary_20200814a.py
@@ -0,0 +1,482 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 2
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 3
+After third an.party()
+
+Before one more party()
+So far x property of object is: 4
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/OOP_test_1.py', 'PartyAnimal': <class '__main__.PartyAnimal'>}
+After an = PartyAnimal()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/OOP_test_1.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7f480701b668>}
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 2
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 3
+After third an.party()
+
+Before one more party()
+So far x property of object is: 4
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'party', 'x']
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+<class '__main__.PartyAnimal'>
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'party', 'x']
+<class 'int'>
+<class 'method'>
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+<class '__main__.PartyAnimal'>
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'party', 'x']
+<class 'int'>
+<class 'method'>
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+an data type:  <class '__main__.PartyAnimal'>
+an methods and properties:  ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'party', 'x']
+an x property data type:  <class 'int'>
+an party method data type:  <class 'method'>
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+an data type:  <class '__main__.PartyAnimal'>
+an methods and properties:  ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'party', 'x']
+an x property data type:  <class 'int'>
+an party method data type:  <class 'method'>
+{}
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+{'x': 1}
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+{}
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+{'x': 1}
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 102
+After third an.party()
+
+Before one more party()
+So far x property of object is: 103
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 101
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 1
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 1
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> an
+<__main__.PartyAnimal object at 0x7f1c347616a0>
+>>> vars(an)
+{'x': 202}
+>>> type(an)
+<class '__main__.PartyAnimal'>
+>>> an.__del
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    an.__del
+AttributeError: 'PartyAnimal' object has no attribute '__del'
+>>> an.__del()
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    an.__del()
+AttributeError: 'PartyAnimal' object has no attribute '__del'
+>>> an.__del__()
+I am destructed 202
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/OOP_test_1.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7f1c347616a0>}
+>>> vars(an)
+{'x': 202}
+>>> an = 900
+I am destructed 202
+>>> an = 900
+>>> vars(an)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    vars(an)
+TypeError: vars() argument must have __dict__ attribute
+>>> an
+900
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 1
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> an
+<__main__.PartyAnimal object at 0x7fd8166e56a0>
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/OOP_test_1.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7fd8166e56a0>}
+>>> vars(an)
+{'x': 202}
+>>> an = 900
+I am destructed 202
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 1
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/OOP_test_1.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7fd7a19f4668>}
+>>> an
+<__main__.PartyAnimal object at 0x7fd7a19f4668>
+>>> vars(an)
+{'x': 202}
+>>> an = 900
+I am destructed 202
+>>> 
+=============== RESTART: /home/user/EDIBO/Python/OOP_test_1.py ===============
+Before an = PartyAnimal()
+I am constructed
+After an = PartyAnimal()
+Before first an.party()
+So far x property of object is: 1
+After first an.party()
+I am constructed
+
+Before second an.party()
+So far x property of object is: 1
+After second an.party()
+
+Before third an.party()
+So far x property of object is: 201
+After third an.party()
+
+Before one more party()
+So far x property of object is: 202
+After one more party()
+>>> an
+<__main__.PartyAnimal object at 0x7f3bf4e54668>
+>>> vars(an)
+{'x': 202}
+>>> x=900
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': '/home/user/EDIBO/Python/OOP_test_1.py', 'PartyAnimal': <class '__main__.PartyAnimal'>, 'an': <__main__.PartyAnimal object at 0x7f3bf4e54668>, 'x': 900}
+>>> an = 900
+>>> 
