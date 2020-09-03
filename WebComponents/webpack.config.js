0416948c2eaf404d87f55f9dae3d789f842c2c95
user <danielaveldze@gmail.com>
2020-09-03 19:38:42 +0300

    20200903_19_37

---
+++ WebComponents/webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = ({ mode }) => {
+  return {
+    mode,
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html' 
+      }),
+    ],
+    devtool: mode === 'development' ? 'source-map' : 'none'
+  };
+};
